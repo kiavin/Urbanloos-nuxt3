7bc4fc07a58aa04b2c13f99a54621f86a08ebc6c
Itachi <bscs157j2022@students.tum.ac.ke>
2025-05-13 21:36:30 +0300

    included the floating whataspp button

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,17 @@
 <script setup>
 import NavBar from '~/components/NavBar.vue'
 import FooterSection from '~/components/FooterSection.vue'
+import FloatingWhatsApp from '~/components/FloatingWhatsApp.vue'
 </script>
 <template>
     <div>
         <NavBar />
         <NuxtPage />
+        <FloatingWhatsApp v-if="$route.path !== '/contact'" />
         <FooterSection />
     </div>
 </template>
+
+
+
+
