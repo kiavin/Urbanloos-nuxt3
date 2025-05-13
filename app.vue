c8990e05bb2e9b2c43c0061e0bc162db9038335c
Itachi <bscs157j2022@students.tum.ac.ke>
2025-05-13 18:48:36 +0300

    Update root app.vue structure

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(()=> {
+  document.addEventListener('copy', function (e) {
+  e.preventDefault();
+  alert('Copying is disabled!');
+});
+})
+
+</script>
 <template>
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
 </template>
+<style>
+
+</style>
